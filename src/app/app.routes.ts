9b9a522b8303f84e9da8b381128d091288ee12da
RaiDiaz <76229344+RaiDiaz@users.noreply.github.com>
2025-06-03 10:56:43 -0500

    refactor: update app configuration and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './features/products/pages/product-list/product-list.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'products', pathMatch: 'full' },
+    { path: 'products', component: ProductListComponent }
+];
